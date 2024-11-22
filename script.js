05d22868a985ba8c49d119bab2e678a658548f4b
Mantasha <siddikimantasha644@gmail.com>
2024-11-22 20:47:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function a(){
+    window.open("veg.html","_self");
+}
+function b(){
+    window.open("fruit.html","_self");
+}
+function c(){
+    window.open("india.html","_self");
+}
+function d(){
+    window.open("sweet.html","_self");
+}
+function e(){
+    window.open("south.html","_self");
+}
+function f(){
+    window.open("street.html","_self");
+}
